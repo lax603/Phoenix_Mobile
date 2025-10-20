46b6501d5fbdfd41847d85e01c17b35a40edd67d
Laxsan Dev <111586431+lax603@users.noreply.github.com>
2025-10-20 10:55:14 +0530

    Add store creation API endpoint
    Implement POST route to create a new store with validation.

---
+++ app/api/store/create/route.js
@@ -0,0 +1,45 @@
+import {getAuth} from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+// create a new store
+export async function POST(request) {
+  try {
+    const {userId} = getAuth(request);
+    // get the data from the from
+    const formatData = await request.formatData();
+
+    const name = formatData.get("name");
+    const username = formatData.get("username");
+    const description = formatData.get("description");
+    const email = formatData.get("email");
+    const contact = formatData.get("contact");
+    const address = formatData.get("address");
+    const image = formatData.get("image");
+
+    if(!name || !username || !description || !email || !contact || !address || !image){
+        return NextResponse.json({message: "missing store info"}, {status: 400});
+    }
+
+    // check the user have already registered a store
+    const store = await prisma.store.findFirst({
+        where: {userId: userId}
+    })
+
+    // if store is already registered then send stuatus of store
+    if(store){
+        return NextResponse.json({status: store.status});
+    }
+
+    // check user name is already taken
+    const isUsernameTaken = await prisma.store.findFirst({
+        where: {username: username.toLowerCase()}
+    })
+
+    if(isUsernameTaken){
+        return NextResponse.json({error: "username is already taken"}, {status: 400});
+    }
+  
+    } catch (error) {
+
+  }
+
+}
